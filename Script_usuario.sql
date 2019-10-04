869d52a81a428dc51888f44c21685b84bf0b1827
Micaela Visser <42984098+MicaVsr@users.noreply.github.com>
2019-10-03 21:28:52 -0300

    cambio codigo por rol

--- Script_usuario.sql
+++ Script_usuario.sql
@@ -1,5 +1,5 @@
 create database gauchoRocket;
 use gauchoRocket;
-create table usuario (id int primary key auto_increment, nombre varchar(50) not null, mail varchar(50) not null, password varchar(15) not null, codigo int not null);
-insert into  usuario (nombre, mail, password, codigo) values ("admin", "admin@gauchorocket.com", "cohete123", 2);
+create table usuario (id int primary key auto_increment, nombre varchar(50) not null, mail varchar(50) not null, password varchar(15) not null, rol int not null, nivel_vuelo int);
+insert into  usuario (nombre, mail, password, rol) values ("admin", "admin@gauchorocket.com", "cohete123", 2);
 select * from usuario;
