459e3fc2dbe6fb8fd8968471eb3ea138fcbd71b0
Luis Blotta <lblotta@THUBAN.soasa.net>
2019-10-10 08:16:25 -0300

    actualizo_script

--- Script_usuario.sql
+++ Script_usuario.sql
@@ -1,5 +1,8 @@
 create database gauchoRocket;
 use gauchoRocket;
-create table usuario (id int primary key auto_increment, nombre varchar(50) not null, mail varchar(50) not null, password varchar(15) not null, rol int not null, nivel_vuelo int);
-insert into  usuario (nombre, mail, password, rol) values ("admin", "admin@gauchorocket.com", "cohete123", 2);
-select * from usuario;
+create table login (id int primary key auto_increment, nick varchar(50) not null, password varchar(50) not null);
+create table usuario (id int primary key auto_increment, nombre varchar(50) not null,  mail varchar(50) not null, rol int not null, nivel_vuelo int, login int not null, foreign key(login) references login(id));
+insert into  login (nick, password) values ("admin", "cohete123");
+select id from login where nick = "admin";
+insert into  usuario (nombre, mail, rol, login) values ("admin", "admin@gauchorocket.com", 2,1) ;
+select * from usuario join login on usuario.login = login.id;
