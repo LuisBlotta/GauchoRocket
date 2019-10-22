530f8b9edd921191327e2ec98a605bad62512a5f
MVillalbaUnlam <43183392+MVillalbaUnlam@users.noreply.github.com>
2019-10-22 15:07:22 -0300

    Fix nombre de campos

--- Script.sql
+++ Script.sql
@@ -1,43 +1,43 @@
-drop database gauchoRocket;
+﻿drop database gauchoRocket;
 create database gauchoRocket;
 use gauchoRocket;
-create table login (id int primary key auto_increment,
+create table login (id_login int primary key auto_increment,
 					userConfirmado boolean not null,
                     hashConfirmacion varchar(50) not null,
 					nick varchar(50) not null,
                     password varchar(50) not null);
                     
-create table usuario (id int primary key auto_increment, 
+create table usuario (id_usuario int primary key auto_increment, 
 						nombre varchar(50) not null,
                         mail varchar(50) not null,
                         rol int not null,
                         nivel_vuelo int,
-                        loginID int not null,
-                        foreign key(loginID) references login(id));
+                        fk_login int not null,
+                        foreign key(fk_login) references login(id_login));
 
 
 insert into  login (userConfirmado, hashConfirmacion, nick, password) values (true,"f50686d5dc72f5d073c5295937bc58ce","admin", "e67732763718fbafa22f23adb5679c2f");
-insert into  usuario (nombre, mail, rol, loginID) values ("admin", "admin@gauchorocket.com", 2,1) ;                      
+insert into  usuario (nombre, mail, rol, fk_login) values ("admin", "admin@gauchorocket.com", 2,1) ;                      
                         
-create table tipo_vuelo (id int primary key, descripcion varchar(20));                        
-create table modelo (id int primary key, descripcion varchar(20) , tipo_vuelo int not null, foreign key(tipo_vuelo) references tipo_vuelo(id));
-create table cabina (id_modelo int, descripcion varchar(20) not null, capacidad int not null, primary key (id_modelo,descripcion), foreign key (id_modelo) references modelo(id));
-create table nivel_pasajero (id_modelo int, numero int not null, primary key (id_modelo,numero), foreign key (id_modelo) references modelo(id));
-create table equipo (id int primary key auto_increment,modelo int not null, matricula varchar(10) not null, foreign key(modelo)references modelo(id));
+create table tipo_vuelo (id_tipo_vuelo int primary key, descripcion varchar(20));                        
+create table modelo (id_modelo int primary key, descripcion varchar(20) , fk_tipo_vuelo int not null, foreign key(fk_tipo_vuelo) references tipo_vuelo(id_tipo_vuelo));
+create table cabina (fk_id_modelo int, descripcion varchar(20) not null, capacidad int not null, primary key (fk_id_modelo, descripcion), foreign key (fk_id_modelo) references modelo(id_modelo));
+create table nivel_pasajero (fk_id_modelo int, id_numero int not null, primary key (fk_id_modelo, id_numero), foreign key (fk_id_modelo) references modelo(id_modelo));
+create table equipo (id_equipo int primary key auto_increment, fk_modelo int not null, matricula varchar(10) not null, foreign key(fk_modelo)references modelo(id_modelo));
 
 
-create table destino (id int primary key, descripcion varchar(20) not null);
-create table tipo_viaje (id int primary key, descripcion varchar(20));
-create table trayecto (id int primary key auto_increment, punto_partida int not null, punto_llegada int not null, duracion int not null, foreign key(punto_partida) references destino(id), foreign key(punto_llegada) references destino(id));
-create table vuelo (id int primary key auto_increment, equipo int not null, trayecto int not null, tipo_viaje int not null, hora_partida int not null, dia_partida date not null, precio int not null, foreign key(tipo_viaje) references tipo_viaje(id) ,foreign key(equipo) references equipo(id),foreign key(trayecto) references trayecto(id));
+create table destino (id_destino int primary key, descripcion varchar(20) not null);
+create table tipo_viaje (id_tipo_viaje int primary key, descripcion varchar(20));
+create table trayecto (id_trayecto int primary key auto_increment, fk_punto_partida int not null, fk_punto_llegada int not null, duracion int not null, foreign key(fk_punto_partida) references destino(id_destino), foreign key(fk_punto_llegada) references destino(id_destino));
+create table vuelo (id_vuelo int primary key auto_increment, fk_equipo int not null, fk_trayecto int not null, fk_tipo_viaje int not null, hora_partida int not null, dia_partida date not null, precio int not null, foreign key(fk_tipo_viaje) references tipo_viaje(id_tipo_viaje) ,foreign key(fk_equipo) references equipo(id_equipo),foreign key(fk_trayecto) references trayecto(id_trayecto));
 
 /*Tablas reserva
-create table reserva (id int primary key auto_increment,nro_reserva int not null, vuelo int not null, usuario int not null);*/
+create table reserva (id_reserva int primary key auto_increment,nro_reserva int not null, vuelo int not null, usuario int not null);*/
 
-INSERT INTO tipo_vuelo (id, descripcion) values (1,"Orbital"),(2,"Baja aceleración"),(3,"Alta aceleración");
-INSERT INTO modelo (id, descripcion, tipo_vuelo) values (1, "Aguila",3), (2, "Aguilucho",2), (3, "Calandria",1), (4, "Canario",2), (5, "Carancho",2), (6, "Colibri",1), (7, "Condor",3), (8, "Guanaco",3), (9, "Halcon",3), (10, "Zorzal",2);
+INSERT INTO tipo_vuelo (id_tipo_vuelo, descripcion) values (1,"Orbital"),(2,"Baja aceleración"),(3,"Alta aceleración");
+INSERT INTO modelo (id_modelo, descripcion, fk_tipo_vuelo) values (1, "Aguila",3), (2, "Aguilucho",2), (3, "Calandria",1), (4, "Canario",2), (5, "Carancho",2), (6, "Colibri",1), (7, "Condor",3), (8, "Guanaco",3), (9, "Halcon",3), (10, "Zorzal",2);
 
-INSERT INTO cabina (id_modelo, descripcion, capacidad) values (1, "G", 200), (1, "F", 75), (1, "S", 25) ,
+INSERT INTO cabina (fk_id_modelo, descripcion, capacidad) values (1, "G", 200), (1, "F", 75), (1, "S", 25) ,
 															  (2, "G", 0), (2, "F", 50), (2, "S", 10),	
                                                               (3, "G", 200), (3, "F", 75), (3, "S", 25),
                                                               (4, "G", 0), (4, "F", 70), (4, "S", 10),	
@@ -47,7 +47,7 @@ INSERT INTO cabina (id_modelo, descripcion, capacidad) values (1, "G", 200), (1,
                                                               (8, "G", 0), (8, "F", 0), (8, "S", 100),	
                                                               (9, "G", 150), (9, "F", 25), (9, "S", 25),	
                                                               (10, "G", 50), (10, "F", 50), (10, "S", 0);	
-INSERT INTO nivel_pasajero (id_modelo, numero) values (1,2), (1,3),
+INSERT INTO nivel_pasajero (fk_id_modelo, numero) values (1,2), (1,3),
 													  (2,2), (2,3),
 													  (3,1), (3,2), (3,3),
 													  (4,2), (4,3),
@@ -57,7 +57,7 @@ INSERT INTO nivel_pasajero (id_modelo, numero) values (1,2), (1,3),
                                                       (8,3),
                                                       (9,3),
                                                       (10,2), (10,3);
-INSERT INTO equipo (modelo, matricula) values   (1, "AA1"),(1, "AA5"), (1, "AA9"), (1, "AA13"), (1, "AA17"),
+INSERT INTO equipo (fk_modelo, matricula) values   (1, "AA1"),(1, "AA5"), (1, "AA9"), (1, "AA13"), (1, "AA17"),
 											    (2, "BA8"), (2, "BA9"), (2, "BA10"), (2, "BA11"), (2, "BA12"),
 												(3, "O1"), (3, "O2"), (3, "O6"), (3, "O7"),
 												(4, "BA13"), (4, "BA14"), (4, "BA15"), (4, "BA16"), (4, "BA17"),
@@ -70,10 +70,10 @@ INSERT INTO equipo (modelo, matricula) values   (1, "AA1"),(1, "AA5"), (1, "AA9"
                                                                      
 select * from cabina join modelo on cabina.id_modelo = modelo.id join nivel_pasajero on modelo.id = nivel_pasajero.id_modelo join equipo on equipo.modelo = modelo.id;
                                                                      
-INSERT INTO destino (id, descripcion) values (1, "BA"), (2, "AK"),(3,"EEI"),(4,"Orbital Hotel"), (5, "Luna"), (6,"Marte"),(7,"Ganimedes"), (8, "Europa"), (9, "Io"), (10, "Encelado"), (11, "Titan");                                                                    
-INSERT INTO tipo_viaje (id, descripcion) values (1, "Suborbital"), (2, "Tour"), (3,"Entre destinos");         
+INSERT INTO destino (id_destino, descripcion) values (1, "BA"), (2, "AK"),(3,"EEI"),(4,"Orbital Hotel"), (5, "Luna"), (6,"Marte"),(7,"Ganimedes"), (8, "Europa"), (9, "Io"), (10, "Encelado"), (11, "Titan");                                                                    
+INSERT INTO tipo_viaje (id_tipo_viaje, descripcion) values (1, "Suborbital"), (2, "Tour"), (3,"Entre destinos");         
 
-INSERT INTO trayecto (punto_partida, punto_llegada, duracion) values (1, 1, 8), /*orbitales*/
+INSERT INTO trayecto (fk_punto_partida, fk_punto_llegada, duracion) values (1, 1, 8), /*orbitales*/
 																	     (2, 2, 8),
 																		 (1, 1, 8),
 																		 (2, 2, 8),
@@ -88,7 +88,7 @@ INSERT INTO trayecto (punto_partida, punto_llegada, duracion) values (1, 1, 8), 
 																		 (7, 10, 118);/*C2 entre destinos AA de ganimedes a encedalo*/
 
 
-INSERT INTO vuelo (equipo, tipo_viaje, trayecto, hora_partida, dia_partida, precio) values 	(3, 1, 1, 12, '20191001', 800), /*orbitales*/
+INSERT INTO vuelo (fk_equipo, fk_trayecto, fk_tipo_viaje, hora_partida, dia_partida, precio) values 	(3, 1, 1, 12, '20191001', 800), /*orbitales*/
 																							(24, 1, 2, 15, '20191001', 800),
 																							(11, 1, 3, 17, '20191002', 800),
 																							(25, 1, 4, 18, '20191002', 800),
