ce308edd26886f8bb502a339966b36cfc5606646
Micaela Visser <42984098+MicaVsr@users.noreply.github.com>
2019-10-28 00:29:01 -0300

    Estilos a info_vuelo y más datos

--- info_vuelo.php
+++ info_vuelo.php
@@ -14,19 +14,26 @@
                 include("mostrar-info-vuelo.php");
                 foreach ($vuelos as $vuelo) {
                     echo "
-                    <article class='info'>
-                        <div  class='card' style='width:300px'>
-    					    <img class='card-img-top' src='public/img/".$vuelo['destino'].".jpg' alt='Card image' style='width:100%''>
-						    <div class='card-body'>
-							    <h2>" . $vuelo['destino'] . "</h2>	
-							    <h4>" . $vuelo['tipo_viaje'] . "</h4>										
-							    <p class='card-text'>Origen: " . $vuelo['origen'] . "</p>
-							    <p class='card-text'><img src='public/img/calendar.png'> " . $vuelo['fecha_ida'] . "</p>				
-						    </div>
-							<a href='reservar-form.php?id_vuelo=".$vuelo['id_vuelo']."' class='btn btn-info stretched-link'>Reservar</a>
-					    </div>
-					</article>";
-                }
+                        <article class='card cont-info'>                        
+                           <img class='card-img-top' src='public/img/".$vuelo['destino'].".jpg' alt='vuelo'>
+                            <div class='card-body'>                                
+                                <h2>" . $vuelo['destino'] . "</h2>  
+                                <h4>" . $vuelo['tipo_viaje'] . "</h4><br>                                       
+                                <p>Origen: " . $vuelo['origen'] . "</p>
+                                <p><img src='public/img/calendar.png'> " . $vuelo['fecha_ida'] . "</p>
+                                <p>Nivel requerido:"; 
+                                foreach ($vuelos as $vuelo) {
+                                    echo "<span class='num-nivel'>".$vuelo['nivel_pasajero']."</span>";
+                                }
+                    echo"       </p>
+                                <br>
+                                <h2>$" . $vuelo['precio'] . "</h2>                                
+                                <a href='reservar-form.php?id_vuelo=".$vuelo['id_vuelo']."' class='btn-reservar btn btn-info'>Reservar</a>
+                            </div>                            
+                        </article>";                        
+                    break; 
+
+                }                
             ?>
         </section>
     </main>
