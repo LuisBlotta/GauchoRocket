e7bed7840a365dc0d33dbe19c132e10177faa281
MVillalbaUnlam <43183392+MVillalbaUnlam@users.noreply.github.com>
2019-11-04 22:15:57 -0300

    update reserva turno

--- modelo/modelo_reserva_turno.php
+++ modelo/modelo_reserva_turno.php
@@ -8,17 +8,20 @@ function getTurnos()
     $id_medico = $_GET['id_medico'];
     $nombre = $_POST['nombre'];
     $fecha_turno = $_POST['fecha_turno'];
-    //$nick = $_COOKIE["login"];
 
     $conn = getConexion();
 
-    /*traigo los turnos x fecha y centro medico*/
-
-    $sql = "SELECT turno.fecha fecha, turno.fk_login usuario, medico.nombre centro_medico
+    $sql = "SELECT turno.fecha fecha, medico.nombre centro_medico
             FROM turno JOIN medico ON medico.id_medico = turno.fk_medico
             WHERE medico.id_medico = '$id_medico' AND turno.fecha = '$fecha_turno'";
 
     $result = mysqli_query($conn, $sql);
+
+    $queryConsulta ="SELECT id_login FROM login WHERE nick='$nombre'";
+
+    $resultlogin = mysqli_query($conn, $queryConsulta);
+    $dato=mysqli_fetch_row($resultlogin);
+
     /* ---------- id_medico ----------
                 Buenos Aires N° - 1 300 turnos x dia
                 Shanghai N° 2 - 210 turnos x dia
@@ -26,35 +29,40 @@ function getTurnos()
 
     if ($id_medico == 1) {
         if (mysqli_num_rows($result) < 3) {
-            $sqlInsert = "INSERT INTO turno (fecha, fk_medico, fk_login)
-                            values ('$fecha_turno', '$id_medico', 1)";
+            $sqlInsert = "INSERT INTO turno (fecha, nick, fk_medico, fk_login)
+                            values ('$fecha_turno', '$nombre', '$id_medico', $dato[0])";
 
-            $resultado = mysqli_query($conn, $sqlInsert);
+            mysqli_query($conn, $sqlInsert);
             mysqli_close($conn);
-
-
+            echo ("Reserva exitosa!");
+            /*echo $sqlInsert;
+            exit();*/
         } else
             echo("Los turnos para el día seleccionado están llenos");
     } elseif ($id_medico == 2) {
 
         if (mysqli_num_rows($result) < 2) {
-            $sqlInsert = "INSERT INTO turno (fecha, fk_medico, fk_login)
-                            values ('$fecha_turno', '$id_medico', 1)";
+            $sqlInsert = "INSERT INTO turno (fecha, nick, fk_medico, fk_login)
+                            values ('$fecha_turno', '$nombre', '$id_medico', $dato[0])";
 
-            $resultado = mysqli_query($conn, $sqlInsert);
+            mysqli_query($conn, $sqlInsert);
             mysqli_close($conn);
-
+            echo ("Reserva exitosa!");
+            /*echo $sqlInsert;
+            exit();*/
 
         } else
             echo("Los turnos para el día seleccionado están llenos");
     } else
         if (mysqli_num_rows($result) < 2) {
-            $sqlInsert = "INSERT INTO turno (fecha, fk_medico, fk_login)
-                            values ('$fecha_turno', '$id_medico', 1)";
+            $sqlInsert = "INSERT INTO turno (fecha, nick, fk_medico, fk_login)
+                            values ('$fecha_turno', '$nombre', '$id_medico', $dato[0])";
 
-            $resultado = mysqli_query($conn, $sqlInsert);
+            mysqli_query($conn, $sqlInsert);
             mysqli_close($conn);
-
+            echo ("Reserva exitosa!");
+            /*echo $sqlInsert;
+            exit();*/
 
         } else
             echo("Los turnos para el día seleccionado están llenos");
