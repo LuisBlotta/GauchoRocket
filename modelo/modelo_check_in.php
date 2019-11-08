a29127707c5b928591a1169ae979db3bc2a06f0d
luisblotta <luis-blotta@hotmail.com>
2019-11-08 09:43:19 -0300

    update_consulta

--- modelo/modelo_check_in.php
+++ modelo/modelo_check_in.php
@@ -5,8 +5,10 @@ chech_in();
      $conn = getConexion();
      $nro_reserva = $_GET['nro_reserva'];
 
+    ### Trae capacidad de de la cabina ###
 
-    $sqlTraeCapacidad="select cabina.capacidad from reserva join vuelo_trayecto on reserva.fk_id_vuelo_trayecto = vuelo_trayecto.id_vuelo_trayecto 
+
+    $sqlTraeCapacidad="select cabina.capacidad, vuelo.dia_partida, vuelo.hora_partida from reserva join vuelo_trayecto on reserva.fk_id_vuelo_trayecto = vuelo_trayecto.id_vuelo_trayecto 
 						join vuelo on vuelo.id_vuelo = vuelo_trayecto.fk_vuelo
                         join equipo on equipo.id_equipo = vuelo.fk_equipo
                         join modelo on modelo.id_modelo = equipo.fk_modelo
@@ -15,7 +17,8 @@ chech_in();
 																							WHERE reserva.nro_reserva  =$nro_reserva);";
 
      $result = mysqli_query($conn, $sqlTraeCapacidad);
-     /*echo $sqlTraeCapacidad;
+     $dato=mysqli_fetch_assoc($result);
+     /*print_r($dato);
      exit();*/
 
 
