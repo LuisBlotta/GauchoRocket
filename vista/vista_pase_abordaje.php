435705eaeaabc0fe93358af9d6381b706718439a
MicaVsr <MicaVsr@users.noreply.github.com>
2019-11-13 14:24:21 -0300

    Fix pase abordaje

--- vista/vista_pase_abordaje.php
+++ vista/vista_pase_abordaje.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
+include ("condicional_sesion.php");
 	$qr=getQR();
-
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Pase de Abordaje</title>
+    <?php include("head.php"); ?>
 </head>
 <body>
 	<?php echo '<img src="'.$qr.'" /><hr/>'; ?>
