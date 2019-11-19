64979a9f7c4bc4877733ed2d2cff6b4b19d04a37
MicaVsr <MicaVsr@users.noreply.github.com>
2019-11-18 21:09:24 -0300

    Fix

--- controlador/controlador_validar_fecha_check_in.php
+++ controlador/controlador_validar_fecha_check_in.php
@@ -1,7 +1,7 @@
 <?php
-    include ("modelo/modelo_fecha_actual.php");
+    include_once("modelo/modelo_fecha_actual.php");
         function fecha_actual_index(){}
-    include ("modelo/modelo_hora_actual.php");
+    include_once("modelo/modelo_hora_actual.php");
         function hora_actual_index(){}
 
     include("modelo/modelo_validar_fecha_check_in.php");
