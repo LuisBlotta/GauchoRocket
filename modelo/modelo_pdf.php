f55db447d9f48436166f36dee2c0f6b3d089c0f2
luisblotta <luis-blotta@hotmail.com>
2019-11-26 16:40:06 -0300

    update_library

--- modelo/modelo_pdf.php
+++ modelo/modelo_pdf.php
@@ -1,9 +1,9 @@
 
 <?php
-require_once 'dompdf2/lib/html5lib/Parser.php';
-require_once 'dompdf2/lib/php-font-lib/src/FontLib/Autoloader.php';
-require_once 'dompdf2/lib/php-svg-lib/src/autoload.php';
-require_once 'dompdf2/src/Autoloader.php';
+require_once 'dompdf/lib/html5lib/Parser.php';
+require_once 'dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
+require_once 'dompdf/lib/php-svg-lib/src/autoload.php';
+require_once 'dompdf/src/Autoloader.php';
 
 Dompdf\Autoloader::register();
 
