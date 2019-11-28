e4eb65171841699825aab2068845f9f7883d4c54
MicaVsr <MicaVsr@users.noreply.github.com>
2019-11-28 19:08:59 -0300

    Fix index y resultado_busqueda

--- vista/vista_resultado_busqueda.php
+++ vista/vista_resultado_busqueda.php
@@ -12,37 +12,23 @@
         <?php
 
         foreach ($vuelos as $vuelo){
-            if ($vuelo['tipo_viaje']=="Tour"){
-                echo "
-                        <a href='info_vuelo?id_vuelo=".$vuelo['id_vuelo']."&id_trayecto=".$vuelo['id_trayecto']."&destino=".$vuelo['destino']."&id_vuelo_trayecto=".$vuelo['id_vuelo_trayecto']."' class='card'>
-                            <img class='card-img-top' src='public/img/".$vuelo['destino'].".jpg' alt='Card image' style='width:100%''>
-                            <div class='card-body'>
-                                <h2>Tour</h2>
-                                <p class='card-text'>Origen: " . $vuelo['origen'] . "</p>
-                                <p class='card-text'><img src='public/img/calendar.png'> " . $vuelo['fecha_ida'] . "</p>				
-                            </div>			
-                        </a>";
+            echo "
+            <a href='info_vuelo?id_vuelo=".$vuelo['id_vuelo']."&id_trayecto=".$vuelo['id_trayecto']."&destino=".$vuelo['destino']."&id_vuelo_trayecto=".$vuelo['id_vuelo_trayecto']."' class='card'>
+                <img class='card-img-top' src='public/img/".$vuelo['destino'].".jpg' alt='Card image' style='width:100%''>
+                <div class='card-body'>";
+
+            if ($vuelo['tipo_viaje']=="Entre destinos"){
+                echo"<h2>" . $vuelo['destino'] . "</h2>";
+            }elseif ($vuelo['tipo_viaje']=="Tour"){
+                echo "<h2>Tour</h2>";
             }elseif ($vuelo['tipo_viaje']=="Suborbital"){
-                echo "
-                        <a href='info_vuelo?id_vuelo=".$vuelo['id_vuelo']."&id_trayecto=".$vuelo['id_trayecto']."&destino=".$vuelo['destino']."&id_vuelo_trayecto=".$vuelo['id_vuelo_trayecto']."' class='card'>
-                            <img class='card-img-top' src='public/img/".$vuelo['destino'].".jpg' alt='Card image' style='width:100%''>
-                            <div class='card-body'>
-                                <h2>Vuelo Suborbital</h2>
-                                <p class='card-text'>Origen: " . $vuelo['origen'] . "</p>
-                                <p class='card-text'><img src='public/img/calendar.png'> " . $vuelo['fecha_ida'] . "</p>				
-                            </div>			
-                        </a>";
-            }else{
-                echo "
-                        <a href='info_vuelo?id_vuelo=".$vuelo['id_vuelo']."&id_trayecto=".$vuelo['id_trayecto']."&destino=".$vuelo['destino']."&id_vuelo_trayecto=".$vuelo['id_vuelo_trayecto']."' class='card'>
-                            <img class='card-img-top' src='public/img/".$vuelo['destino'].".jpg' alt='Card image' style='width:100%''>
-                            <div class='card-body'>
-                                <h2>" . $vuelo['destino'] . "</h2>
-                                <p class='card-text'>Origen: " . $vuelo['origen'] . "</p>
-                                <p class='card-text'><img src='public/img/calendar.png'> ".$vuelo['fecha_ida']."</p>				
-                            </div>			
-                        </a>";
+                echo "<h2>Vuelo Suborbital</h2>";
             }
+
+            echo"<p class='card-text'>Origen: " . $vuelo['origen'] . "</p>
+                <p class='card-text'><img src='public/img/calendar.png'> " . $vuelo['fecha_ida'] . "</p>				
+                </div>			
+            </a>";
         }
         ?>
     </section>
