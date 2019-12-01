e2161667725aff5777cf11a4f51022b3839dacd2
MicaVsr <MicaVsr@users.noreply.github.com>
2019-12-01 14:58:38 -0300

    Del campos email y nick

--- vista/vista_reserva_form.php
+++ vista/vista_reserva_form.php
@@ -28,13 +28,10 @@ include_once("conexion.php");
     <section class="cont-form_reserva">
         <h3>Reserva de pasaje</h3>
         <?php echo"<form method='post' action='reserva?id_vuelo=$id_vuelo&id_trayecto=$id_trayecto&id_destino=$id_destino&id_vuelo_trayecto=$id_vuelo_trayecto&id_origen=$id_origen&fecha_ida=$fecha_ida'> "?>
-
-        <div class="form-row">
             <div class="form-group">
                 <label for="pasajeros">Cantidad de pasajeros</label>
                 <input type="number" class="form-control" name="cant_pasajeros" id="cant_pasajeros" required placeholder="N° de pasajeros" value="1">
             </div>
-
             <div class="form-group">
                 <label for="cabina">Cabina</label>
                 <select name="cabina" class="custom-select">
@@ -43,16 +40,6 @@ include_once("conexion.php");
                     <option value="S">Suite</option>
                 </select>
             </div>
-        </div>
-
-        <label for="nombre">Nombre</label>
-        <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre">
-
-
-        <label for="mail">E-mail</label>
-        <input type="text" class="form-control" name="mail" id="mail" placeholder="E-mail">
-
-        <br>
         <button class="btn btn-info">Reservar</button>
         </form>
     </section>
